869c87e69fedf48f00c2e0f2e13a08854ac36c15
ydechmi <yacine.dechmi@gmail.com>
2015-05-23 22:22:51 +0200

    Add new project: spring-boot-jar-demo

---
+++ spring-boot-jar-demo/src/main/java/com/yd/demo/SpringBootJarDemoApplication.java
@@ -0,0 +1,12 @@
+package com.yd.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJarDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJarDemoApplication.class, args);
+    }
+}
