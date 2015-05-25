5c2dce98575c35f906726bf08798aa9380938f01
ydechmi <yacine.dechmi@gmail.com>
2015-05-25 22:11:23 +0200

    Spring Controller of welcome page

---
+++ spring-boot-jar-demo/src/main/java/com/yd/demo/web/WelcomeController.java
@@ -0,0 +1,17 @@
+package com.yd.demo.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by yacine on 25/05/2015.
+ */
+@Controller
+public class WelcomeController {
+
+    @RequestMapping("/welcome")
+    public String welcome() {
+
+        return "/welcome.html";
+    }
+}
