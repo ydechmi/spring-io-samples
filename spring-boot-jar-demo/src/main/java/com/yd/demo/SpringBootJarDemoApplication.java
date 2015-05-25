b4ebd1ff10d169b126fcef50f2a6e73dc2997ffa
ydechmi <yacine.dechmi@gmail.com>
2015-05-25 18:37:08 +0200

    Implementing main() method

--- spring-boot-jar-demo/src/main/java/com/yd/demo/SpringBootJarDemoApplication.java
+++ spring-boot-jar-demo/src/main/java/com/yd/demo/SpringBootJarDemoApplication.java
@@ -2,11 +2,22 @@ package com.yd.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class SpringBootJarDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootJarDemoApplication.class, args);
+        ApplicationContext ctx=SpringApplication.run(SpringBootJarDemoApplication.class, args);
+
+        System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
     }
 }
